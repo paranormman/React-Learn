e42ba9cd3d98a9a6f2154d899d8bf890d22ce2f2
Manoj Shiv <manojshivaprakash10@gmail.com>
2025-02-16 10:14:23 +0530

    React basics

--- Week1-Basics/script.js
+++ Week1-Basics/script.js
@@ -1,2 +1,19 @@
-const x = "Manoj";
-console.log(x);
+// const x = "Manoj";
+// console.log(x);
+
+// const obj = {
+//     name: "Manoj",
+//     age: 26
+// };
+
+// console.log(obj.age, obj.name);
+
+const x = 7;
+
+const y = "-7";
+
+console.log(x === y);
+
+// const y = Boolean(x);
+// console.log(y);
+// console.log(typeof y);
