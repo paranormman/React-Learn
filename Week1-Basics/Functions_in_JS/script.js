3215228c8afa16faf178c62577dcf737693a8a16
Manoj Shiv <manojshivaprakash10@gmail.com>
2025-02-22 23:53:23 +0530

    JS functions

---
+++ Week1-Basics/Functions_in_JS/script.js
@@ -0,0 +1,41 @@
+// function greet(firstName){
+//     console.log(firstName, "Hi, How are you?")
+
+//     return firstName + " Hello";
+// }
+
+// const res = greet("Manoj");
+// console.log(res);
+
+
+function multiply(a, b) {
+    console.log("Multiply " + a + " and " + b);
+    console.log(a * b);
+    // return a * b;
+}
+multiply(2, 3)
+// const res = multiply(5, 2);
+// console.log(res);
+
+// console.log(multiply(5, 10));
+
+// Arrow Function
+
+const addition = (c, d) => {
+    console.log("Adding " + c + " and " + d);
+    console.log(c + d);
+    // return c + d;
+};
+
+addition(10, 1);
+// console.log(addition(9, 1));
+
+// const greetFunction = function greet(){
+//     console.log("hi");
+// }
+
+// greetFunction();
+
+function greet() {
+    console.log("hi");
+} greet();
