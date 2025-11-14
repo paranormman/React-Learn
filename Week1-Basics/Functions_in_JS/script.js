e1542bfb67e7021f911874aa153c857e09e74868
Manoj Shiv <manojshivaprakash10@gmail.com>
2025-11-14 16:05:33 +0530

    Learning Function

--- Week1-Basics/Functions_in_JS/script.js
+++ Week1-Basics/Functions_in_JS/script.js
@@ -62,14 +62,14 @@
 // function add(a, b) {
 //     return a + b;
 // }
-// call a function
+// // call a function
 // function mul(a, b) {
 //     return a * b;
 // }
 
 // const res = calculate(10, 10 , add);
 // console.log(res);
-// calling the function using arrow method
+// // calling the function using arrow method
 // const resMul = calculate(2, 4 , (a, b) => a*b);
 // console.log(resMul);
 
@@ -89,17 +89,58 @@
 // console.log(res2);
 
 // This function in JS
-console.log(this);
+// console.log(this);
 
-function greet() {
-    console.log(this);
-}; greet();
+// function greet() {
+//     console.log(this);
+// }; greet();
 
-const obj ={
-    age: 30,
-    walk() {
-        console.log(this);
-    },
-};
-obj.walk();
+// const obj ={
+//     age: 30,
+//     walk() {
+//         console.log(this);
+//     },
+// };
+// obj.walk();
 
+// const multiple = (a) => {
+//     return (b) => a * b; 
+// }
+
+// console.log(multiple);
+
+// const double = multiple(2)
+// const res = double(5);
+// console.log(res);
+
+// console.log(this);
+
+// function greet() {
+//     console.log(this);
+// }
+
+// greet();
+
+// const obj = {
+//     age: 34,
+//     walk() {
+//         console.log(this);
+//     },
+// };
+
+// obj.walk();
+
+// calculator using multiple functions
+
+const calculator = (a, b, operation) => {
+    return operation(a, b)
+}
+
+const resadd = calculator(2, 4, (a, b) => (a + b));
+console.log(resadd);
+const ressub = calculator(5, 4, (a, b) => (a - b));
+console.log(ressub);
+const resmul = calculator(4, 5, (a, b) => (a * b));
+console.log(resmul);
+const resdiv = calculator(10, 5, (a, b) => (a / b));
+console.log(resdiv);
